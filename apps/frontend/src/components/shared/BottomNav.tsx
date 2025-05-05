c1c1f00d98e8d37b815e8dcdbfe6d67d9687751b
Cristian Perez <cperezlecaros@gmail.com>
2025-05-05 18:46:35 -0400

    Create bottom nav and home ui

---
+++ apps/frontend/src/components/shared/BottomNav.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import { BiSolidDish } from "react-icons/bi";
+import { CiCircleMore } from "react-icons/ci";
+import { FaHome } from "react-icons/fa";
+import { MdOutlineReorder, MdTableBar } from "react-icons/md";
+
+const BottomNav: FC = () => {
+  return (
+    <div
+      className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 h-16 
+    flex justify-around"
+    >
+      <button
+        className="flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px]
+      rounded-[20px]"
+      >
+        <FaHome className="inline mr-3}" size={30} /> <p>Home</p>
+      </button>
+      <button className="flex items-center justify-center text-[#ababab] w-[200px]">
+        <MdOutlineReorder className="inline mr-3}" size={30} /> <p>Órdenes</p>
+      </button>
+      <button className="flex items-center justify-center text-[#ababab] w-[200px]">
+        <MdTableBar className="inline mr-3}" size={30} /> <p>Mesas</p>
+      </button>
+      <button className="flex items-center justify-center text-[#ababab] w-[200px]">
+        <CiCircleMore className="inline mr-3}" size={30} /> <p>Más</p>
+      </button>
+
+      <button className="absolute bottom-6 bg-[#F6B100] text-[#f5f5f5] rounded-full p-3 items-center">
+        <BiSolidDish size={30} />
+      </button>
+    </div>
+  );
+};
+export default BottomNav;
--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,6 +1,15 @@
 import { FC } from "react";
+import BottomNav from "../components/shared/BottomNav";
 
 const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <section className="bg-[#1f1f1f] h-[calc(100vh-5.75rem)] overflow-hidden flex gap-3">
+      {/*LEFT DIV */}
+      <div className="flex-[3] bg-[#1a1a1a]"></div>
+      {/*RIGHT DIV */}
+      <div className="flex-[2] bg-[#121212]"></div>
+      <BottomNav />
+    </section>
+  );
 };
 export default Home;
