d3536dfa60a3de6b9a5d6c83dcd20a11c028cd32
Cristian Perez <cperezlecaros@gmail.com>
2025-05-06 20:55:54 -0400

    Create Greetings component

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
 import BottomNav from "../components/shared/BottomNav";
+import Greetings from "../components/home/Greetings";
 
 const Home: FC = () => {
   return (
     <section className="bg-[#1f1f1f] h-[calc(100vh-5.75rem)] overflow-hidden flex gap-3">
       {/*LEFT DIV */}
-      <div className="flex-[3] bg-[#1a1a1a]"></div>
+      <div className="flex-[3] bg-[#1a1a1a]">
+        <Greetings />
+      </div>
       {/*RIGHT DIV */}
       <div className="flex-[2] bg-[#121212]"></div>
       <BottomNav />
