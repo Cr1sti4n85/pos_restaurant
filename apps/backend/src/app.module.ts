df77a9854d036fe9e1e023d6abb32e530385661d
Cristian Perez <cperezlecaros@gmail.com>
2025-05-20 14:51:54 -0400

    Generate user and auth resources

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,12 +5,14 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: join(__dirname, '../../../.env'),
     }),
     MongooseModule.forRootAsync({
       inject: [ConfigService],
@@ -23,6 +25,8 @@ import { MongooseModule } from '@nestjs/mongoose';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../', 'frontend/dist'),
     }),
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
